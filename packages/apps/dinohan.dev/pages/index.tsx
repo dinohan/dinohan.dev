4023a1a636b2e3e099bd4f5613b4e3c72e186da9
dinohan <dinohan.dev@gmail.com>
2023-02-27 20:56:34 +0900

    features(next) add dinohan.dev next app

---
+++ packages/apps/dinohan.dev/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import { NextPage } from 'next'
+
+
+const IndexPage: NextPage = () => {
+  return (
+    <main>
+      <Head>
+        <title>Dino</title>
+      </Head>
+
+      <h1>Hi, I'm Dino</h1>
+    </main>
+  )
+}
+
+export default IndexPage
